a001464d0f17cceb9bbbd79054626fe006ff2d06
leonard01 <leonard.collins1983@gmail.com>
2025-03-23 15:38:55 +0700

    basic structure

---
+++ bin/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const { program } = require('commander');
+
+// Set up basic metadata
+program
+  .name('mycli')
+  .description('A simple CLI built with Commander')
+  .version('1.0.0');
+
+// Define a command: greet
+program
+  .command('greet')
+  .description('Prints a greeting')
+  .option('-n, --name <string>', 'Name of the person to greet', 'World')
+  .action((options) => {
+    console.log(`Hello, ${options.name}!`);
+  });
+
+// Parse command-line arguments
+program.parse(process.argv);
